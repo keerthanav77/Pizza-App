de3b8356a2f11f2aca24ab191a0167c418262a6c
keerthanav77 <regikeerthana3@gmail.com>
2024-10-25 23:10:09 +0530

    Main file updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MainApp());
+};
+
+class  extends StatelessWidget {
+  const ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
+
+
+
+
 
 
 
